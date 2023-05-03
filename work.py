18ecf13989df6aa4e2000598c076fc6f2571bbf5
Stephen Telian <stevietelian@gmail.com>
2023-05-03 22:22:50 +0300

    Remove unecessary files

--- work.py
+++
@@ -1,9 +0,0 @@
-from Assets.DataHandling.FileHandling.Read import Read
-from Assets.DataHandling.FileHandling.Write import Write
-
-
-file = Read("data.json").extract()
-result = []
-for unit in file["units"]:
-    result.append(unit['title'])
-Write('','new.json', result).dump()

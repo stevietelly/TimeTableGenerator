3f8344ad40602ed3fb609c6d43ec401f0469eba0
Stephen Telian <stevietelian@gmail.com>
2023-05-03 22:10:58 +0300

    Echo function to allow printing 0 to the screen with --echo flag

---
+++ Assets/Functions/Echo.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+class Echo:
+    state: bool = False
+    def __init__(self) -> None:
+        """
+        A class to approve printing specific values to the screen
+        """
+        pass
+    def print(self, *value: Any):
+        if self.state:
+            print(value)

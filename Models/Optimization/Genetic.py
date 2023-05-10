fef7ede4f1d272f2b92acc0d300424a9a6f39ed6
Stephen Telian <telianstephen@gmail.com>
2023-05-10 23:09:39 +0300

    Changed Evaluation module to Fitness for clarity

--- Models/Optimization/Genetic.py
+++ Models/Optimization/Genetic.py
@@ -1,57 +1,69 @@
 """
 Genetic Optimization Algorithim
-
-Process
-Initialization
-fitness evaluation
-Selection (Rank-based selection)
-crossover
-mutation
-fitness evaluation
-replacement
-termination
 """
+from Assets.Functions.Echo import Echo
+from Data.Parsers.Data import DataReader
 from Logic.Structure.Configuration import Configuration
 from Logic.Structure.Session import Session
-from Models.Evaluation.Evaluation import FitnessEvaluation
+from Models.Evaluation.Fitness import FitnessEvaluation
 from Models.Optimization.Annealing import Annealing
+from Models.Randomizer.Randomizer import Randomizer
 from Objects.Persons.Students import Group
 from Objects.Academic.Units import Unit
 from Objects.Physical.Rooms import Room
 from Objects.Persons.Instructor import Instructor
-from Objects.Academic.Programme import Course
+from Objects.Academic.Programme import Programme
 from Logic.Structure.Timetable import Timetable
 from typing import List
 from Models.Generator.Generator import Generator
 from Objects.User.Priorities.Priorities import Priorities
 
 class Genetic:
-    def __init__(self, configuration: Configuration, instructors: List[Instructor], rooms: List[Room], units: List[Unit], courses: List[Course], groups: List[Group]):
-        self.initial_population = 60
-        self.parent_selection_size = 2
+    """
+    Process
+        - Initialization
+        - fitness evaluation
+        - Selection (Rank-based selection)
+        - crossover
+        - mutation
+        - fitness evaluation
+        - replacement
+        - termination
+    """
+    def __init__(self, inputData: DataReader, parent_size: int = 2, initial_population: int = 6):
+        self.initial_population = initial_population
+        self.parent_selection_size = parent_size
 
-        self.configuration: Configuration = configuration
-        self.instructors = instructors
-        self.rooms = rooms
-        self.units = units
-        self.courses = courses
-        self.groups = groups
+        self.configuration: Configuration = inputData.configuration
+        self.inputData = inputData
+        self.instructors = inputData.instructors
+        self.rooms = inputData.rooms
+        self.units = inputData.units
+        self.courses = inputData.programmes
+        self.groups = inputData.groups
         self.priorities: Priorities = self.configuration.priorities
-
         self.population: List[Timetable] = []
+
+        self.echo = Echo()
+
+        self.echo.print(f'Running a Genetic Algorithim with {self.initial_population} initial population and a parent size of {self.parent_selection_size}')
     
     def Intialise(self):
+        self.echo.print("\n")
+        self.echo.print("Creating Initial Population")
         for _ in range(self.initial_population):
-            generator = Generator(self.configuration, self.instructors, self.rooms, self.units, self.courses, self.groups)
+            generator = Generator(self.inputData)
             generator.Scheduling()
             generator.Accessioning()
             timetable = generator.generate()
             self.population.append(timetable)
             
     def Fitness(self):
+        self.echo.print("\n")
+        self.echo.print(f"Evaluating Fitness of {len(self.population)} Population")
         evaluated = []
         for timetable in self.population:
-            fitness = FitnessEvaluation(timetable, self.rooms, self.groups, self.instructors, self.priorities)
+            fitness = FitnessEvaluation(timetable, self.inputData)
             fitness.maximum_consecutive_sessions = self.configuration.consecutive
             fitness.soft_constraint_satisfaction_acceptance = self.configuration.soft_constraints_satisfaction_rate
             fitness.Evaluate()
@@ -62,13 +74,22 @@ class Genetic:
         self.population = evaluated
     
     def Selection(self):
+        self.echo.print("\n")
+        self.echo.print(f"Selecting Most fit from population")
+        self.echo.print(f" {len(self.population)}  Original Population")
+
         self.population = self.population[:int(len(self.population)/2)]
+        self.echo.print(f"Reduced to {len(self.population)} Population")
+
     
     def Crossover(self):
+        self.echo.print("\n")
+        self.echo.print(f"Crossover a population of {len(self.population)}")
+
         timetables = []
         for i in range(0, len(self.population)):
             j = i + 1
-            if not j > len(self.population):
+            if not j >= len(self.population):
                 parent_1 = self.population[i]
                 parent_2 = self.population[j]
 
@@ -80,7 +101,8 @@ class Genetic:
                     child_session = self.reproduce(session_1, session_2)
                   
                 timetables.append(child_timetable)
-            self.population = timetables
+        self.population = timetables
+        self.echo.print(f"Reduced {len(self.population)} Population")
 
     def reproduce(self, parent_1: Session, parent_2: Session)->Session:
         new_session: Session = parent_2
@@ -91,10 +113,12 @@ class Genetic:
         return new_session
 
     def Mutation(self) :
+        self.echo.print(f"\n")
+        self.echo.print(f"Mutating {len(self.population)}")
         mutated_population: List[Session] = []
         for timetable in self.population:
-            anneal = Annealing(timetable)
-            anneal.Optimize()
+            anneal = Randomizer(timetable)
+            anneal.Random()
             
             mutated_population.append(anneal.Output())
         self.population = mutated_population
